c89e1f2a2e2d4e4fff496d56e0bad81a09e955ef
Nestor Castelblanco <145400867+nestorcastelblanco@users.noreply.github.com>
2026-03-02 08:27:22 -0500

    icono

---
+++ app/src/main/java/com/example/seguimiento1/features/home/HomeScreen.kt
@@ -0,0 +1,48 @@
+package com.example.seguimiento1.features.home
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.seguimiento1.R
+
+@Composable
+fun HomeScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.ic_reportes_ciudadanos),
+            contentDescription = "Logo Reportes Ciudadanos",
+            modifier = Modifier.size(150.dp)
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Text(
+            text = "Reportes Ciudadanos",
+            fontSize = 28.sp,
+            style = MaterialTheme.typography.headlineMedium
+        )
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        Text(
+            text = "Tu plataforma para informar y dar seguimiento a incidencias en tu comunidad de manera rápida y efectiva.",
+            fontSize = 16.sp,
+            textAlign = TextAlign.Center,
+            color = MaterialTheme.colorScheme.onSurfaceVariant
+        )
+    }
+}
